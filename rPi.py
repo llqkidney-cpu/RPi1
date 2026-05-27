657687445f399c7ab57853d6f843375ebac25423
llqkidney-cpu <llqkidney@gmail.com>
2026-05-27 15:38:07 +0000

    first

---
+++ rPi.py
@@ -0,0 +1,11 @@
+from rtlsdr import RtlSdr
+
+sdr = RtlSdr()
+sdr.sample_rate = 2.4e6
+sdr.center_freq = 100e6
+sdr.gain = 'auto'
+
+samples = sdr.read_samples(256 * 1024)
+print(f"Read {len(samples)} samples")
+
+sdr.close()
